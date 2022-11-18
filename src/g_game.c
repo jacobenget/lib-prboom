1f7b1557fa95b76bbe4bf817da66ebb413694b3c
Jacob Enget <jacob.enget@gmail.com>
2022-11-18 17:44:56 -0600

    Fixed some warnings
    Initialize some local variables that are sometimes referenced later
    without otherwise being assigned a value

--- src/g_game.c
+++ src/g_game.c
@@ -2146,7 +2146,7 @@ void G_InitNew(skill_t skill, int episode, int map) {
 #define DEMOMARKER 0x80
 
 void G_ReadDemoTiccmd(ticcmd_t *cmd) {
-  unsigned char at; // e6y: tasdoom stuff
+  unsigned char at = 0; // e6y: tasdoom stuff
 
   if (*demo_p == DEMOMARKER)
     G_CheckDemoStatus(); // end of demo data stream
@@ -2218,7 +2218,7 @@ void G_RecordDemo(const char *name) {
     demofp = fopen(demoname, "r+");
     if (demofp) {
       int slot = -1;
-      int rc;
+      int rc = 0;
       int bytes_per_tic;
       const byte *pos;
 
@@ -2448,7 +2448,7 @@ void G_BeginRecording(void) {
   /* cph - 3 demo record formats supported: MBF+, BOOM, and Doom v1.9 */
   if (mbf_features) {
     { /* Write version code into demo */
-      unsigned char v;
+      unsigned char v = 0;
       switch (compatibility_level) {
       case mbf_compatibility:
         v = 203;
@@ -2504,7 +2504,7 @@ void G_BeginRecording(void) {
       *demo_p++ = 0;
 
   } else if (compatibility_level > boom_compatibility_compatibility) {
-    byte v, c; /* Nominally, version and compatibility bits */
+    byte v = 0, c = 0; /* Nominally, version and compatibility bits */
     switch (compatibility_level) {
     case boom_compatibility_compatibility:
       v = 202, c = 1;
