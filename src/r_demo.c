c1186e4386db9c06863600c11c1a2f7e668d31ea
Jacob Enget <jacob.enget@gmail.com>
2024-02-21 17:03:22 -0600

    Fixed 2 [-Waddress] warnings
    by removing boolean checks on addresses that
    are provably non-zero, and hence always resolve to 'true'

--- src/r_demo.c
+++ src/r_demo.c
@@ -45,7 +45,7 @@ static int smooth_playing_index;
 static angle_t smooth_playing_angle;
 
 void R_SmoothPlaying_Reset(player_t *player) {
-  if (demo_smoothturns && demoplayback && players) {
+  if (demo_smoothturns && demoplayback) {
     if (!player)
       player = &players[displayplayer];
 
